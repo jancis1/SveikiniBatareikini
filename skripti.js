9f4e400398f335ab03f5717b9e0e581a8ba7c879
jancis1 <zigats.janis@gmail.com>
2023-02-07 11:09:23 +0200

    Update skripti.js

--- skripti.js
+++ skripti.js
@@ -6,5 +6,8 @@ var vards =  document.querySelector("#vards");
 var zina = document.querySelector("#zina");
 
 function sutitZinu(){
-    console.log("Poga darbojas")}
+    console.log("Poga darbojas")
     console.log("ziņa ir: "+zina.value);
+
+    log.innerHTML = log.innerHTML +"♥"+ zina.value;
+}
